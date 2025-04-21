160ef0b6acb2f9d70e0003d7164a1f93bb4f1682
Kristen Thayer <kristenmthayer@gmail.com>
2025-04-21 09:05:51 -0700

    first part of class: variables, type coercion, arrays

--- lecture_07_javascript/index.js
+++ lecture_07_javascript/index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+console.log("hello world!!!!!")
+console.log("this executes second")
+
+let myVar = 'hello!'
+myVar = 35
+//myVar = true
+
+let noVal
+
+console.log(myVar, typeof(myVar))
+
+console.log('40' + 2)
+console.log(2 + '40')
+let myString = '42'
+
+console.log(myString, +myString);
+
+const lettersArr = ['a', 'b', 'c']
+lettersArr.push('newVal')
+
+console.log(lettersArr, lettersArr.length)
