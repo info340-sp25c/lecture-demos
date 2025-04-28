ffecd726a777a502cc828786ea7d44aedc60e002
Kristen Thayer <kristenmthayer@gmail.com>
2025-04-28 09:18:21 -0700

    update javascript

--- lecture_09_dom/js/index.js
+++ lecture_09_dom/js/index.js
@@ -8,14 +8,20 @@ const songArray = [
   }
 ];
 
-const phoneDigits = [8, 6, 7, 5, 3, 0, 9]
+// const phoneDigits = [8, 6, 7, 5, 3, 0, 9]
 
-const largestDigit = phoneDigits.reduce(function(prev, curr){
-  if (prev > curr){
-    return prev
-  } else {
-    return curr
-  }
-})
+// const largestDigit = phoneDigits.reduce(function(prev, curr){
+//   if (prev > curr){
+//     return prev
+//   } else {
+//     return curr
+//   }
+// })
 
-console.log("largest digit", largestDigit)
+// console.log("largest digit", largestDigit)
+
+const newParagraph = document.createElement('p');
+newParagraph.textContent = "this is content from my index.js"
+
+const mainElem = document.querySelector('main')
+mainElem.appendChild(newParagraph)
