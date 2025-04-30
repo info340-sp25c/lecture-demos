5af4eb28715ffb0d67f611fbc628937e3e22efd4
Kristen Thayer <kristenmthayer@gmail.com>
2025-04-30 09:46:12 -0700

    MORE javascript!!! from class demos

--- lecture_10_es6/js/index.js
+++ lecture_10_es6/js/index.js
@@ -8,3 +8,32 @@ const monsterArray = [
   {name: 'Creature from the Black Lagoon', type: 'fish', height: 71}
 ]
 console.log(monsterArray);
+
+const sayHello = (name) => {
+  return "Hello " + name;
+}
+
+console.log(sayHello("class!"))
+
+const updateMonsters = monsterArray.map((monster) => {
+ // debugger;
+  return monster.name + ", my NEW friend!"
+})
+
+console.log(updateMonsters);
+
+const myArray = [1, 2, 3]
+const [x, y, za] = myArray
+
+console.log(x, y, za)
+
+const myObject = {a: 1, b: 2, c: 3}
+const {a, b, d} = myObject
+
+console.log(a, b, d)
+
+const monsterCopy = {...monsterArray[0]}
+console.log(monsterCopy)
+
+const monster2Copy = {...monsterArray[0], Height: 75, alternateForm: "bat", age: 150}
+console.log(monster2Copy);
