949a04a561102bf1b0316b83439258a9a9784885
Kristen Thayer <kristenmthayer@gmail.com>
2025-05-05 09:28:47 -0700

    first class demo getting react to work

--- lecture_11_react_intro/src/main.jsx
+++ lecture_11_react_intro/src/main.jsx
@@ -2,3 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+console.log("jsx file");
+
+      // const msgElem = document.createElement('h1')
+      // msgElem.id = "hello";
+      // msgElem.classList.add('myClass');
+      // msgElem.textContent = 'Hello World!';
+      // document.getElementById('root').appendChild(msgElem)
+
+// const msgElem = React.createElement(
+//     'h1',
+//     { id: "hello", className: "myClass" },
+//     "Hello World FROM REACT main.jsx!"
+// )
+const myName = "Kristen"
+const leapYear = (365 + 1) * 24 * 60 
+const myImg = '/img/puppy.jpg'
+const msgElem = (
+    <>
+        <header>
+            <h1 id="hello" className="myClass">Hello World! (from {myName})</h1>
+            <p>A leap year has { leapYear } minutes!</p>
+            <img src={myImg} alt = "a puppy" />
+        </header>
+        <footer>
+            This is a footer
+        </footer>
+    </>
+)
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(msgElem)
