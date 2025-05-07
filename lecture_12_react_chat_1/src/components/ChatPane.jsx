fc8707920112da420a0fbbb2421618ce4a5d479a
Kristen Thayer <kristenmthayer@gmail.com>
2025-05-07 10:03:36 -0700

    added count to state

--- lecture_12_react_chat_1/src/components/ChatPane.jsx
+++ lecture_12_react_chat_1/src/components/ChatPane.jsx
@@ -1,7 +1,10 @@
-const handleClick = (event) => {
-    console.log("look, I clicked on ", event.target)
-}
+import { useState } from "react"
 
 export function ChatPane(props){
-    return <button onClick={handleClick}>Click Me!</button>
+    const [count, setCount] = useState(0) 
+    const handleClick = (event) => {
+        console.log("look, I clicked on ", event.target)
+        setCount(count + 1)
+    }
+    return <button onClick={handleClick}>Click Me! (clicked {count} times)</button>
 }
