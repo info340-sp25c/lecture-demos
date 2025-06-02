0f9a04a2dbdd3215d6c22541a98f8e3983163fec
Kristen Thayer <kristenmthayer@gmail.com>
2025-06-02 09:09:00 -0700

    firebase init (USE YOUR OWN COFIG PLEASE!)

--- lecture_18_react_chat_firebase/src/main.jsx
+++ lecture_18_react_chat_firebase/src/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 
 import { BrowserRouter } from 'react-router'
 
@@ -8,6 +13,20 @@ import App from './components/App.jsx';
 //import CSS
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDf3znXTAMgWnj31XeKEy3ljpxMe4i2Y6k",
+  authDomain: "class-demo-fb.firebaseapp.com",
+  databaseURL: "https://class-demo-fb-default-rtdb.firebaseio.com",
+  projectId: "class-demo-fb",
+  storageBucket: "class-demo-fb.firebasestorage.app",
+  messagingSenderId: "453368934476",
+  appId: "1:453368934476:web:221ac9d2b37a460d5a44e9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
